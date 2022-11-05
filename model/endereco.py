44b85051c20fa7a0c2f9d43457b2cc9e6d55b298
Gleidson Magalhães <89873935+gleidsoncbmf@users.noreply.github.com>
2022-11-05 10:48:56 -0300

    Mapeamento

--- model/endereco.py
+++ model/endereco.py
@@ -1,4 +1,17 @@
+from helpers.database import db
+
 class Endereco():
+    
+     __tablename__ = "tb_endereco"
+
+    id = db.Column(db.Integer, primary_key=True)
+    cep = db.Column(db.String(10), nullable=False)
+    complemento = db.Column(db.String(30), nullable=False)
+    referencia = db.Column(db.String(300), nullable=False)
+    logradouro = db.Column(db.String(100), nullable=False)
+
+    pessoa_parent = db.Column(db.Integer, db.ForeignKey("tb_pessoa.id"))
+    instituicaoDeEnsino_parent = db.Column(db.Integer, db.ForeignKey("tb_instituicaoDeEnsino.id"))
 
     def __init__(self, cep, numero, complemento, referencia, logradouro):
         self.cep = cep
