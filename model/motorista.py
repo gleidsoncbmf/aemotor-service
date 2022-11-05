72464c0157b11b850bda575d07975bfc3a0533da
Gleidson Magalhães <89873935+gleidsoncbmf@users.noreply.github.com>
2022-11-05 10:57:11 -0300

    Mapeamento

--- model/motorista.py
+++ model/motorista.py
@@ -1,6 +1,17 @@
 from model.funcionario import Funcionario
+from helpers.database import db
 
-class Motorista(Funcionario):
+
+class Motorista(Funcionario, db.Model):
+    
+    __tablename__ = "tb_motorista"
+    __mapper_args__ = {'polymorphic_identity': 'motorista', 'concrete': True}
+
+    id = db.Column(db.Integer, primary_key=True)
+    rotas = db.Column(db.String(80), nullable=False)
+    
+    funcionario_parent = db.Column(db.Integer, db.ForeignKey("tb_funcionario.id"))
+    veiculo_child = db.relationship('Veiculo',uselist=False)
 
     def __init__(self, nome, nascimento, email, telefone, prefeitura, cargo, rotas):
         super().__init__(nome, nascimento, email, telefone, prefeitura, cargo)
