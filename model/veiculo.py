5208fdcd3f53a4144c3314cac6b1c990915695ef
Gleidson Magalhães <89873935+gleidsoncbmf@users.noreply.github.com>
2022-11-05 11:10:05 -0300

    Mapeamento

--- model/veiculo.py
+++ model/veiculo.py
@@ -1,4 +1,16 @@
-class Veiculo ():
+from helpers.database import 
+
+class Veiculo (db.Model):
+    
+    __tablename__ = "tb_veiculo"
+
+    id = db.Column(db.Integer, primary_key=True)
+    cidade = db.Column(db.String(80), nullable=False)
+    qtdPassageiros = db.Column(db.String(10), nullable=False)
+    tipoVeiculo = db.Column(db.String(20), nullable=False)
+    placa = db.Column(db.String(20), nullable=False)
+
+    motorista_parent = db.Column(db.Integer, db.ForeignKey("tb_motorista.id"))
 
     def __init__(self, cidade, qtdPassageiros, tipoVeiculo, placa):
         self.cidade = cidade
