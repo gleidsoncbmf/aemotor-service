eb088f90acc8c98c28694032e2107b5a24e5ca65
Gleidson Magalhães <89873935+gleidsoncbmf@users.noreply.github.com>
2022-11-05 11:01:11 -0300

    Mapeamento

--- model/prefeito.py
+++ model/prefeito.py
@@ -1,6 +1,19 @@
 from model.pessoa import Pessoa
+from helpers.database import db
 
-class Prefeito(Pessoa):
+
+class Prefeito(Pessoa, db.Model):
+    
+     __tablename__ = "tb_prefeito"
+    __mapper_args__ = {'polymorphic_identity': 'prefeito', 'concrete': True}
+
+    id = db.Column(db.Integer, primary_key=True)
+    nomePrefeito = db.Column(db.String(80), nullable=False)
+
+    pessoa_parent_ = db.Column(db.Integer, db.ForeignKey("tb_pessoa.id"))
+    prefeitura_parent = db.Column(db.Integer, db.ForeignKey("tb_prefeitura.id"))
+
+    pessoa = db.relationship("Pessoa",foreign_keys=[pessoa_parent_])
 
     def __init__(self, nome, nascimento, email, telefone):
         super().__init__(nome, nascimento, email, telefone)
