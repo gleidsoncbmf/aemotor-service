a2b8893a93c5f144d3a97759cf7ca3023f7d4a8a
Gleidson Magalhães <89873935+gleidsoncbmf@users.noreply.github.com>
2022-11-05 10:42:25 -0300

    Atualização

--- model/aluno.py
+++ model/aluno.py
@@ -1,6 +1,21 @@
 from model.pessoa import Pessoa
+from helpers.database import db
 
 class Aluno(Pessoa):
+    
+    __tablename__ = "tb_aluno"
+    __mapper_args__ = {'polymorphic_identity': 'aluno', 'concrete': True}
+    
+    id = db.Column(db.Integer, primary_key=True)
+    instituicaoDeEnsino = db.Column(db.String(80), nullable=False)
+    curso = db.Column(db.String(50), nullable=False)
+    matricula = db.Column(db.String(20), nullable=False)
+
+    pessoa_parent = db.Column(db.Integer, db.ForeignKey("tb_pessoa.id"))
+    instituicaoDeEnsino_child = db.relationship("InstituicaoDeEnsino", uselist=False)
+    rota = db.relationship('Rota', backref='Rota', lazy=True)
+    passageiro_child = db.relationship('Passageiro',uselist=False)
+    
 
     def __init__(self, nome, nascimento, email, telefone, instituicaoDeEnsino, curso, matricula):
         super().__init__(nome, nascimento, email, telefone)
