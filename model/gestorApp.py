c691c083e32e1f762a95f92f8fe63407a66a65be
Gleidson Magalhães <89873935+gleidsoncbmf@users.noreply.github.com>
2022-11-05 10:52:22 -0300

    Mapeamento

--- model/gestorApp.py
+++ model/gestorApp.py
@@ -1,6 +1,16 @@
 from model.pessoa import Pessoa
+from helpers.database import db
 
-class GestorApp(Pessoa):
+class GestorApp(Pessoa, db_Model):
+    
+    __tablename__ = "tb_gestorApp"
+    __mapper_args__ = {'polymorphic_identity': 'gestor', 'concrete': True}
+
+    id = db.Column(db.Integer, primary_key=True)
+    gestorApp = db.Column(db.String(80), nullable=False)
+
+    pessoa_parent = db.Column(db.Integer, db.ForeignKey("tb_pessoa.id"))
+    prefeitura_parent = db.Column(db.Integer, db.ForeignKey('tb_prefeitura.id'), nullable=False)
 
     def __init__(self, nome, nascimento, email, telefone):
         super().__init__(nome, nascimento, email, telefone)
