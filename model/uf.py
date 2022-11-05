c07a092a5dad8b6288d6676ded0c79a38dfbcbdf
Gleidson Magalhães <89873935+gleidsoncbmf@users.noreply.github.com>
2022-11-05 11:09:01 -0300

    Mapeamento

--- model/uf.py
+++ model/uf.py
@@ -1,4 +1,15 @@
+from helpers.database import db
+
 class Uf():
+    
+    __tablename__ = "tb_uf"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(90), nullable=False)
+    sigla = db.Column(db.String(6), nullable=False)
+
+    cidade_parent = db.Column(db.Integer, db.ForeignKey("tb_cidade.id"))
+
 
     def __init__(self, nome, sigla):
         self.nome = nome
