287c382a4d1a65863d39a3c79785021660df8ef4
Gleidson Magalhães <89873935+gleidsoncbmf@users.noreply.github.com>
2022-11-05 10:50:41 -0300

    Mapeamento

--- model/funcionario.py
+++ model/funcionario.py
@@ -1,6 +1,21 @@
 from model.pessoa import Pessoa
+from helpers.database import db
 
 class Funcionario(Pessoa):
+    
+    __tablename__ = "tb_funcionario"
+    __mapper_args__ = {'polymorphic_identity': 'funcionario', 'concrete': True}
+    
+    id = db.Column(db.Integer, primary_key=True)
+    prefeitura = db.Column(db.String(90), nullable=False)
+    cargo = db.Column(db.String(30), nullable=False)
+
+    
+    pessoa_parent = db.Column(db.Integer, db.ForeignKey("tb_pessoa.id"))
+    prefeitura_id = db.Column(db.Integer, db.ForeignKey('tb_prefeitura.id'), nullable=False)
+    motorista_child = db.relationship("Motorista", uselist=False)
+
+    pessoa = db.relationship("Pessoa",foreign_keys=[pessoa_parent])
 
     def __init__(self,nome, nascimento, email, telefone, prefeitura, cargo):
         super().__init__(nome, nascimento, email, telefone)
