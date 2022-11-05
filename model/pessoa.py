1fa507b7a7b9d738e85b3e94b3f26fb1f51e1796
Gleidson Magalhães <89873935+gleidsoncbmf@users.noreply.github.com>
2022-11-05 10:59:34 -0300

    Mapeamento

--- model/pessoa.py
+++ model/pessoa.py
@@ -1,4 +1,25 @@
+from helpers.database import db
+from sqlalchemy.types import String
+
 class Pessoa():
+    
+     __tablename__ = "tb_pessoa"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    nascimento = db.Column(db.String(10), nullable=False)
+    email = db.Column(db.String(150), nullable=False)
+    telefone = db.Column(db.String(11), nullable=False)
+
+    aluno_child = db.relationship("Aluno", uselist=False)
+    prefeito_child = db.relationship("Prefeito", uselist=False)
+    endereco_child = db.relationship("Endereco", uselist=False)
+    funcionario_child = db.relationship("Funcionario", uselist=False)
+    gestor_child = db.relationship("GestorApp", uselist=False)
+
+    #HERANÇA
+    tipo_pessoa = db.Column('tipo_pessoa', String(50))
+    __mapper_args__ = {'polymorphic_on': tipo_pessoa}
 
     def __init__(self, nome, nascimento, email, telefone):
         self.nome = nome
