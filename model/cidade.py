8fd7b983c3c50602a70681edd8478a9a595ad37a
Gleidson Magalhães <89873935+gleidsoncbmf@users.noreply.github.com>
2022-11-05 10:45:41 -0300

    Mapeamento

--- model/cidade.py
+++ model/cidade.py
@@ -1,4 +1,13 @@
+from helpers.database import db
+
 class Cidade():
+    
+     __tablename__ = 'tb_cidade'
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    
+    prefeitura_child = db.relationship("Prefeitura", uselist=False)
 
     def __init__(self, nome):
         self.nome = nome
