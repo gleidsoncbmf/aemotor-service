1deaf6160ca242241628d730b7938210cbaf48d5
Gleidson Magalhães <89873935+gleidsoncbmf@users.noreply.github.com>
2022-11-05 11:08:08 -0300

    Mapeamento

--- model/rota.py
+++ model/rota.py
@@ -1,4 +1,21 @@
-class Rota():
+from helpers.database import db
+
+class Rota(db.Model):
+    
+    __tablename__ = 'tb_rota'
+
+    id = db.Column(db.Integer, primary_key=True)
+    cidadeDestino = db.Column(db.String(200), nullable=False)
+    qtdAlunos = db.Column(db.String(11), nullable=False)
+    prefeitura = db.Column(db.String(200), nullable=False)
+    veiculo = db.Column(db.String(30), nullable=False)
+    passageiro = db.Column(db.String(50), nullable=False)
+    horaSaida = db.Column(db.DateTime(), nullable=False)
+    horaChegada = db.Column(db.DateTime(), nullable=False)
+
+    prefeitura_child = db.relationship("Prefeitura", uselist=False)
+    instituicoes = db.relationship('InstituicaoDeEnsino', backref='InstituicaoDeEnsino', lazy=True)
+    aluno_parent = db.Column(db.Integer, db.ForeignKey('tb_aluno.id'), nullable=False)
 
     def __init__(self, cidadeDestino, qtdAlunos, veiculo, passageiro, horaSaida, horaChegada):
         self.cidadeDestino = cidadeDestino
